f6895513c935d8d468b595f7c87c3b53970e72fc
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-25 15:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from discord.ext import commands
-b=commands.bot(command_prefix="-")
-b.run()
+b=commands.Bot(command_prefix="-")
+b.run('Nzg5Mzk0MDI4NjQyMDQxODU3.X9xaeg.FuKf3J03E-8bzrAfhAaZ5ke7NxU')
